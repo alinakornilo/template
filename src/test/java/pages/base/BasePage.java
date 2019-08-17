0dbf47e3f2c733f7b22baea00aeb0b167a80afa6
alina <alinakornilo@gmail.com>
2019-08-17 17:48:07 +0300

    Initialized driver instance in base page

--- src/test/java/pages/base/BasePage.java
+++ src/test/java/pages/base/BasePage.java
@@ -1,5 +1,17 @@
 package pages.base;
 
+import org.openqa.selenium.WebDriver;
+import tests.base.BaseTest;
+
 public class BasePage {
 
+  private final WebDriver driver;
+
+  BasePage() {
+    driver = BaseTest.getDriver();
+  }
+
+  public WebDriver getDriver() {
+    return driver;
+  }
 }
