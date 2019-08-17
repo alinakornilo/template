4a875050f6dedea68d1ff083bb63a2d08f970517
alina <alinakornilo@gmail.com>
2019-08-17 17:42:31 +0300

    Added logic to base test

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,5 +1,32 @@
 package tests.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
 public class BaseTest {
 
+  private WebDriver driver;
+
+  public WebDriver getDriver() {
+    return driver;
+  }
+
+  @BeforeClass
+  public static void setupClass() {
+    WebDriverManager.chromedriver().setup();
+  }
+
+  @BeforeTest
+  public void setupTest() {
+    driver = new ChromeDriver();
+  }
+
+  @AfterTest
+  public void teardown() {
+    driver.quit();
+  }
 }
