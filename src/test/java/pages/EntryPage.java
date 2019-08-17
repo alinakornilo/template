814f018c7935167b235bbc2d7f869b6f16e46d48
alina <alinakornilo@gmail.com>
2019-08-17 18:00:16 +0300

    Added elements to the page

--- src/test/java/pages/EntryPage.java
+++ src/test/java/pages/EntryPage.java
@@ -1,7 +1,24 @@
 package pages;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import pages.base.BasePage;
 
 public class EntryPage extends BasePage {
 
+  @FindBy(css = "")
+  private WebElement question;
+  @FindBys(@FindBy(id = ""))
+  private List<WebElement> answers;
+
+  public String getQuestion() {
+    return question.getText();
+  }
+
+  public List<String> getAnswers() {
+    return answers.stream().map(WebElement::getText).collect(Collectors.toList());
+  }
 }
--- src/test/java/tests/EntryTest.java
+++ src/test/java/tests/EntryTest.java
@@ -1,12 +1,16 @@
 package tests;
 
 import org.testng.annotations.Test;
+import pages.EntryPage;
 import tests.base.BaseTest;
 
 public class EntryTest extends BaseTest {
 
   @Test
   public static void entryTest() {
+    EntryPage entryPage = new EntryPage();
+    // String question = entryPage.getQuestion();
+    // List<String> answers = entryPage.getAnswers();
 
   }
 }
